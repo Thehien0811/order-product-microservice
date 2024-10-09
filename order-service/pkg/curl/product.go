40d9747251cf50682733886c5ed6d055d03e1c02
thehien0811 <thehien08112003@gmail.com>
2024-10-09 22:16:22 +0700

    update curl

--- order-service/pkg/curl/product.go
+++ order-service/pkg/curl/product.go
@@ -17,7 +17,7 @@ type ProductResponse struct {
 type Product struct {
 	ID       string `json:"ID"`
 	Name     string `json:"Name"`
-	Quantity int32    `json:"Quantity"`
+	Quantity int32  `json:"Quantity"`
 }
 
 func DetailProduct(id string) (*Product, error) {
@@ -29,7 +29,7 @@ func DetailProduct(id string) (*Product, error) {
 	if resp.StatusCode != http.StatusOK {
 		return nil, err
 	}
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -44,28 +44,27 @@ func DetailProduct(id string) (*Product, error) {
 }
 
 func UpdateProductStock(productID string, quantity int) bool {
-    url := "http://localhost:8002/api/v1/products/" + productID
+	url := "http://localhost:8002/api/v1/products/" + productID
 
-    jsonStr := []byte(fmt.Sprintf(`{"quantity": %d}`, quantity))
+	jsonStr := []byte(fmt.Sprintf(`{"quantity": %d}`, quantity))
 
-    req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(jsonStr))
-    if err != nil {
-        fmt.Println("Error creating request:", err)
-        return false
-    }
+	req, err := http.NewRequest(http.MethodPatch, url, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return false
+	}
 
-    req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Println("Error sending request:", err)
-        return false
-    }
-    defer resp.Body.Close()
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return false
+	}
+	defer resp.Body.Close()
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("Response:", string(body))
-    return true
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Response:", string(body))
+	return true
 }
-
